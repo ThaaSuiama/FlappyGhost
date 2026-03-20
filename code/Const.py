3c7aab48b3280b64fcc06f50b1af4d77bb22eadd
ThaaSuiama <thaasuiama@hotmail.com>
2026-03-20 20:12:12 -0300

    Creating asset folder Inserting all assets Creating the menu Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,31 @@
+#C
+COLOR_RED = (200, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 128)
+
+
+
+#E
+ENTITY_SPEED = {
+    'Level1bg0': 0,
+    'Level1bg1': 1,
+    'Level1bg2': 2,
+    'Level1bg3': 3,
+    'Level1bg4': 4,
+    'Level1bg5': 5,
+    'Level1bg6': 6,
+}
+
+
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+#W
+WIN_WIDTH = 537
+WIN_HEIGHT = 820
+
